cfff1bfc884c02b8d6089353ac1cebf4519feb1b
heiligbasil <basilief@yahoo.com>
2019-06-17 20:27:24 -0700

    Stretch goal: Added a POST integration test for adding a Student

--- school/src/test/java/com/lambdaschool/school/controller/StudentControllerIntegrationTest.java
+++ school/src/test/java/com/lambdaschool/school/controller/StudentControllerIntegrationTest.java
@@ -1,17 +1,33 @@
 package com.lambdaschool.school.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.lambdaschool.school.model.Course;
+import com.lambdaschool.school.model.Student;
+import io.restassured.module.mockmvc.RestAssuredMockMvc;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.web.context.WebApplicationContext;
 
+import static io.restassured.module.mockmvc.RestAssuredMockMvc.given;
 import static org.junit.Assert.*;
 
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class StudentControllerIntegrationTest
 {
+    @Autowired
+    private WebApplicationContext webApplicationContext;
 
     @Before
     public void setUp() throws Exception
     {
+        RestAssuredMockMvc.webAppContextSetup(webApplicationContext);
     }
 
     @After
@@ -20,8 +36,14 @@ public class StudentControllerIntegrationTest
     }
 
     @Test
-    public void addNewStudent()
+    public void addNewStudent() throws JsonProcessingException // POST /students/student/add
     {
+        Student makeStudent = new Student("Billy");
+        makeStudent.setStudid(105);
 
+        ObjectMapper mapper = new ObjectMapper();
+        String constructedJson = mapper.writeValueAsString(makeStudent);
+
+        given().contentType("application/json").body(constructedJson).when().post("/students/student/add").then().statusCode(201);
     }
 }
