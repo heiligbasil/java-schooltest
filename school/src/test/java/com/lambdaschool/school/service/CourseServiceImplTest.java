70efa90cacfc1d02ad5a7641a6b7b6df666585eb
heiligbasil <basilief@yahoo.com>
2019-06-17 19:32:53 -0700

    Added the Save unit test

--- school/src/test/java/com/lambdaschool/school/service/CourseServiceImplTest.java
+++ school/src/test/java/com/lambdaschool/school/service/CourseServiceImplTest.java
@@ -1,6 +1,11 @@
 package com.lambdaschool.school.service;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.lambdaschool.school.SchoolApplication;
+import com.lambdaschool.school.model.Course;
+import com.lambdaschool.school.model.Instructor;
+import com.lambdaschool.school.model.Student;
+import com.lambdaschool.school.repository.InstructorRepository;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -11,7 +16,11 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 import javax.persistence.EntityNotFoundException;
 
+import java.util.ArrayList;
+import java.util.Optional;
+
 import static junit.framework.TestCase.assertEquals;
+import static junit.framework.TestCase.assertNotNull;
 import static org.junit.Assert.*;
 
 @RunWith(SpringRunner.class)
@@ -21,9 +30,19 @@ public class CourseServiceImplTest
     @Autowired
     private CourseService courseService;
 
+    @Autowired
+    private InstructorRepository instructorRepository;
+
+    private ArrayList<Course> courseList;
+
     @Before
     public void setUp() throws Exception
     {
+        courseList = new ArrayList<>();
+        courseList.add(new Course("Science", new Instructor("John")));
+        courseList.add(new Course("Social Studies", new Instructor("Chance")));
+        courseList.add(new Course("Mathematics", new Instructor("Patrick")));
+        courseList.add(new Course("English", new Instructor("Josh")));
     }
 
     @After
@@ -64,5 +83,14 @@ public class CourseServiceImplTest
     @Test
     public void save()
     {
+        //Optional<Instructor> instructor = instructorRepository.findById(1L)/*.findInstructorsByInstructnameEquals("John")*/;
+        Course makeCourse = new Course("History", instructorRepository.findById(1L).get());
+        makeCourse.getStudents().add(new Student("Suzy"));
+
+        Course addCourse = courseService.save(makeCourse);
+
+        assertNotNull(addCourse);
+        Course foundRestaurant = courseService.findCourseById(addCourse.getCourseid());
+        assertEquals(addCourse.getCoursename(), foundRestaurant.getCoursename());
     }
 }
