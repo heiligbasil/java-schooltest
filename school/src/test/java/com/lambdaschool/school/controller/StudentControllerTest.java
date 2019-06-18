0d14e63dbc4f12d17c04468ed4b1f6dd994e307a
heiligbasil <basilief@yahoo.com>
2019-06-17 20:34:08 -0700

    Stretch goal: New Unit Test for AddNewStudent()

--- school/src/test/java/com/lambdaschool/school/controller/StudentControllerTest.java
+++ school/src/test/java/com/lambdaschool/school/controller/StudentControllerTest.java
@@ -1,13 +1,35 @@
 package com.lambdaschool.school.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.lambdaschool.school.model.Student;
+import com.lambdaschool.school.service.StudentService;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 
-import static org.junit.Assert.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+@RunWith(SpringRunner.class)
+@WebMvcTest(value = StudentController.class, secure = false)
 public class StudentControllerTest
 {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private StudentService studentService;
 
     @Before
     public void setUp() throws Exception
@@ -20,8 +42,20 @@ public class StudentControllerTest
     }
 
     @Test
-    public void addNewStudent()
+    public void addNewStudent() throws Exception
     {
+        Student makeStudent = new Student("Winslow");
+        makeStudent.setStudid(67);
+        ObjectMapper mapper = new ObjectMapper();
+        String constructedJson = mapper.writeValueAsString(makeStudent);
 
+        Mockito.when(studentService.save(any(Student.class))).thenReturn(makeStudent);
+
+        RequestBuilder requestBuilder = MockMvcRequestBuilders.post("/students/student/add")
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON)
+                .content(constructedJson);
+
+        mockMvc.perform(requestBuilder).andExpect(status().isCreated()).andDo(MockMvcResultHandlers.print());
     }
 }
